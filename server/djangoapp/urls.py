25a18d0ac3f679d1954c702f94dd5c20998ab51f
dushkoklandev <dushkoklandev16@gmail.com>
2023-03-06 12:36:08 -0500

    user auth -1

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -10,8 +10,10 @@ urlpatterns = [
     # name the URL
 
     # path for about view
+    path(route='about', view=views.about, name='about'),
 
     # path for contact us view
+    path(route='contact', view=views.contact, name='contact'),
 
     # path for registration
 
@@ -20,8 +22,6 @@ urlpatterns = [
     # path for logout
 
     path(route='', view=views.get_dealerships, name='index'),
-    path(route='about', view=views.about, name='about'),
-    path(route='contact', view=views.contact, name='contact')
 
     # path for dealer reviews view
 
